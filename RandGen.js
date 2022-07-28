6e9beacffdcbe3cd907ad96faf0115cddcd25fab
Zeus <107733608+ItsZeusBro@users.noreply.github.com>
2022-07-28 14:12:32 -0400

    Create RandGen.js

---
+++ RandGen.js
@@ -0,0 +1,32 @@
+class RandGen{
+    randStr(){return this.genStr(this.randRange(0, 3))}
+    randInt(){return this.randRange(0,3)}
+    randArr(n){var arr=[]; for(var i=0;i<n;i++){arr.push(this.rand())}; return arr}
+    rand(){
+        return[
+            this.randIntArr, this.randStr, this.randInt, this.randEnc, this.randEncArr, this.randStrArr,
+            this.randObj, this.randObjArr
+        ].sample()()
+    }
+    randIntArr(n=this.randInt()){var arr=[]; for(var i=0;i<n;i++){arr.push(this.randInt())}; return arr}
+    randEnc(){return "utf8"}
+    randEncArr(){return ['utf8']}
+    randStrArr(n=this.randInt()){var arr=[]; for(var i=0;i<n;i++){arr.push(this.randStr())}; return arr}
+    randObj(n=this.randInt()){if(n){return {[this.randStr()]:this.randObj(n-1)}}};
+    randObjArr(n=this.randInt()){var arr=[]; for(var i=0;i<n;i++){arr.push(this.randObj())}; return arr}
+    randKey(guardFuncBag){
+        return guardFuncBag[Math.floor(Math.random() * guardFuncBag.length)];
+    }
+    randRange(min, max){
+        return Math.floor(Math.random()*(max-min+1)+min)
+    }
+    genStr(len, chars='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'){
+        //programiz.com
+        var str='';
+        for (var i = 0; i<len; i++){str+=chars.charAt(Math.floor(Math.random()*chars.length))}
+        return str;
+    }
+    randMod10(){
+        return Math.floor(Math.random()*(100-0+1)+0)%2
+    }
+}
