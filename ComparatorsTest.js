4ce366275c9515060b67caff2c30f6a0ab516652
zeus <zeusofolympus1000@gmail.com>
2022-08-14 14:10:47 -0400

    ComparatorTetst.js passed all tests

--- ComparatorsTest.js
+++ ComparatorsTest.js
@@ -1,93 +1,92 @@
-import { Comparators } from "./Comparators.js"
-import { RandGen } from "../Random/RandGen.js"
+import { Test } from "./Test.js"
 import * as assert from "node:assert"
 import * as util from "node:util"
 
 export class ComparatorTests{
-	constructor(){
-		this.testArrays()
-		this.testObjects()
-		this.testNumber()
-		this.testString()
-		this.testEquals()
+	constructor(n){
+		this.testArrays(n)
+		this.testObjects(n)
+		this.testNumber(n)
+		this.testString(n)
+		this.testEquals(n)
 	}
 
-	testArrays(){
+	testArrays(n){
 		for(var i = 0; i<1000; i++){
-			this.testArray()
+			this.testArray(n)
 		}
 
 	}
 
 
-	testObjects(){
+	testObjects(n){
 		for(var i = 0; i<1000; i++){
-			this.testObject()
+			this.testObject(n)
 		}
 	}
 
-	testArray(){
+	testArray(n){
 		console.log("TEST ARRAY")
-		var comparators = new Comparators()
-		var array1 = new RandGen().randArr(20)
-		var array2 = new RandGen().randArr(20)
-		assert.equal(comparators.isEqualArr(array1.slice(), array1.slice()), true)
-		assert.equal(comparators.isEqualArr(array1.slice(), array2.slice()), false)
+		var test = new Test()
+		var array1 = test.randArr(n)
+		var array2 = test.randArr(n)
+		assert.equal(test.isEqualArr(array1.slice(), array1.slice()), true)
+		assert.equal(test.isEqualArr(array1.slice(), array2.slice()), false)
 		console.log("TEST ARRAY PASSED")
 
 	}
 
-	testObject(){
+	testObject(n){
 		console.log("TEST OBJ")
-		var comparators = new Comparators()
-		var obj1 = new RandGen().randObj(20)
-		var obj2 = new RandGen().randObj(20)
-		assert.equal(comparators.isEqualObj(obj1, obj1), true)
-		assert.equal(comparators.isEqualObj(obj1, obj2), false)
+		var test = new Test()
+		var obj1 = test.randObj(n)
+		var obj2 = test.randObj(n)
+		assert.equal(test.isEqualObj(obj1, obj1), true)
+		assert.equal(test.isEqualObj(obj1, obj2), false)
 		console.log("TEST OBJ PASSED")
 	}
 
-	testNumber(){
+	testNumber(n){
 		console.log("TEST NUMBER")
-		var comparators = new Comparators()
+		var test = new Test()
 		var num1 = 1234
 		var num2 = 2345
-		assert.equal(comparators.isEqualNumber(num1, num1), true)
-		assert.equal(comparators.isEqualNumber(num1, num2), false)		
+		assert.equal(test.isEqualNumber(num1, num1), true)
+		assert.equal(test.isEqualNumber(num1, num2), false)		
 		console.log("TEST NUMBER PASSED")
 	}
-	testString(){
+	testString(n){
 		console.log("TEST STRING")
-		var comparators = new Comparators()
+		var test = new Test()
 		var str1 = "kldsjfhasklgjhsdglaksdjgalukierhtfglkaszjgnaslgkuhaerg"
 		var str2 = "kldsjfhasklgjhsdglaksdjgalukierhtfglkaszjgnaslgkuhaergkldsjfhasklgjhsdglaksdjgalukierhtfglkaszjgnaslgkuhaerg"
-		assert.equal(comparators.isEqualStr(str1, str1), true)
-		assert.equal(comparators.isEqualStr(str1, str2), false)		
+		assert.equal(test.isEqualStr(str1, str1), true)
+		assert.equal(test.isEqualStr(str1, str2), false)		
 		console.log("TEST STRING PASSED")
 	}
-	testEquals(){
+	testEquals(n){
 		console.log("TEST EQUAL")
-		var comparators = new Comparators()
-		var array1 = new RandGen().randArr(20)
-		var array2 = new RandGen().randArr(20)
-		assert.equal(comparators.isEqual(array1.slice(), array1.slice()), true)
-		assert.equal(comparators.isEqual(array1.slice(), array2.slice()), false)
+		var test = new Test()
+		var array1 = test.randArr(n)
+		var array2 = test.randArr(n)
+		assert.equal(test.isEqual(array1.slice(), array1.slice()), true)
+		assert.equal(test.isEqual(array1.slice(), array2.slice()), false)
 
-		var obj1 = new RandGen().randObj(20)
-		var obj2 = new RandGen().randObj(20)
-		assert.equal(comparators.isEqual(obj1, obj1), true)
-		assert.equal(comparators.isEqual(obj1, obj2), false)
+		var obj1 = test.randObj(n)
+		var obj2 = test.randObj(n)
+		assert.equal(test.isEqual(obj1, obj1), true)
+		assert.equal(test.isEqual(obj1, obj2), false)
 
 		var num1 = 1234
 		var num2 = 2345
-		assert.equal(comparators.isEqual(num1, num1), true)
-		assert.equal(comparators.isEqual(num1, num2), false)	
+		assert.equal(test.isEqual(num1, num1), true)
+		assert.equal(test.isEqual(num1, num2), false)	
 
 
 		var str1 = "kldsjfhasklgjhsdglaksdjgalukierhtfglkaszjgnaslgkuhaerg"
 		var str2 = "kldsjfhasklgjhsdglaksdjgalukierhtfglkaszjgnaslgkuhaergkldsjfhasklgjhsdglaksdjgalukierhtfglkaszjgnaslgkuhaerg"
-		assert.equal(comparators.isEqual(str1, str1), true)
-		assert.equal(comparators.isEqual(str1, str2), false)		
+		assert.equal(test.isEqual(str1, str1), true)
+		assert.equal(test.isEqual(str1, str2), false)		
 		console.log("TEST EQUAL PASSED")
 	}
 	log(obj){
@@ -97,3 +96,4 @@ export class ComparatorTests{
     }
 
 }
+new ComparatorTests(5)
--- Test.js
+++ Test.js
@@ -1,8 +1,93 @@
-export class Comparators{
-    constructor(){
-        
+export class Test{
+	//RANDOM GENERATORS----------------------------------------------------
+	randStr(n, array=false){
+		if(array){
+			var arr=[]; 
+			for(var i=0;i<n;i++){arr.push(this.genStr(this.randRange(0, n)))}; 
+			return arr;
+		}else{
+			return this.genStr(this.randRange(0, n));
+		}
+	}
+
+    randInt(n, array=false){
+		if(array){
+			var arr=[]; 
+			for(var i=0; i<n; i++){arr.push(this.randRange(0, n))};
+			return arr;
+		}else{
+			return this.randRange(0,n);
+		}
+	}
+
+    randArr(n, array=false){
+		if(array){
+			var arrOfArr=[]
+			for(var i = 0; i<n; i++){
+				var arr = []
+				for(var j=0;j<n;j++){
+					arr.push(this.rand(n))
+				}; 
+				arrOfArr.push(arr)
+			}
+			return arrOfArr
+		}else{
+			var arr=[]; 
+			for(var i=0;i<n;i++){
+				arr.push(this.rand(n))
+			}; 
+			return arr;
+		}
+		
+	}
+    rand(n){return eval(this.sample(['this.randStr', 'this.randInt', 'this.randObj'])+'(n, this.randMod10())')}
+    // randEnc(n){return "utf8"}
+    // randEncArr(n){return ['utf8']}
+    randObj(n, array=false){
+		//O(nlognlogn) growth complexity
+		if(array){
+			var objArr=[]
+			for(var i=0; i<n; i++){
+				objArr.push(this._randObj(n))
+			}
+			return objArr
+		}else{
+			return this._randObj(n)
+		}
+	};
+	_randObj(n){
+		var obj={}
+			if(n){
+				for(var i=0; i<n; i++){
+					obj[this.randStr(20)]=this._randObj(n-1)
+				}
+			}
+			return obj
+	}
+
+    randSelection(bag){
+        return bag[Math.floor(Math.random() * bag.length)];
     }
-    isEqualObj(obj1, obj2, equal=[true]){
+    randRange(min, max){
+        return Math.floor(Math.random()*(max-min+1)+min)
+    }
+    genStr(len, chars='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'){
+        //programiz.com
+        var str='';
+        for (var i = 0; i<len; i++){str+=chars.charAt(Math.floor(Math.random()*chars.length))}
+        return str;
+    }
+    randMod10(){
+        return Math.floor(Math.random()*(100-0+1)+0)%2
+    }
+	sample(arr){
+		return arr[this.randRange(0, arr.length-1)]
+	}
+
+
+	//COMPARATORS----------------------------------------------------
+
+	isEqualObj(obj1, obj2, equal=[true]){
 		if(
 				(!equal[0])
 			||
