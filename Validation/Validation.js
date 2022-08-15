96cd61810f47942c6f61113b2dc453838334266a
zeus <zeusofolympus1000@gmail.com>
2022-08-15 18:23:14 -0400

    n_min n_max for various functions

--- Validation/Validation.js
+++ Validation/Validation.js
@@ -1,32 +1,32 @@
 import { assert } from "node:console";
 export class Validation{
 	//VALIDATION-----------------------------------------------------
-	//gt is greater than n nested levels, lt is less than n nested levels, eq is equal to n nested levels
-	//gn is greater than n objects per strata, ln is less than n objects per strata, en is equal to n objects per strata
-	isStrata(){
-		
-	}
+	//ng means assert that n is greater than ng
+	//nl means assert that n is less than nl
+	//they mean different things in different contexts
+	isInt(n, n_min, n_max){
+		//ng means assert that integer n is greater than ng
+		//nl means assert that integer n is less than nl
 
-	isInt(n, ng, nl){
-		if(n && ng && nl){
+		if(n && n_min && n_max){
 			try{
 				assert.equal(typeof n ==='number', true)
-				assert.equal(n>ng, true)
-				assert.equal(n<nl, true)
+				assert.equal(n>=n_min, true)
+				assert.equal(n<=n_max, true)
 			}catch(err){
 				return false
 			}
-		}else if(n && ng){
+		}else if(n && n_min){
 			try{
 				assert.equal(typeof n ==='number', true)
-				assert.equal(n>ng, true)
+				assert.equal(n>=n_min, true)
 			}catch(err){
 				return false
 			}
-		}else if(n && nl){
+		}else if(n && n_max){
 			try{
 				assert.equal(typeof n ==='number', true)
-				assert.equal(n<nl, true)
+				assert.equal(n<=n_max, true)
 			}catch(err){
 				return false
 			}
@@ -41,27 +41,29 @@ export class Validation{
 		}
 		return true
 	}
-	
-	isArray(arr, ng, nl, nng, nnl){
-		if(arr && ng && nl){
+
+	isArray(arr, n_min, n_max){
+		//ng means assert that array arr.length is greater than ng
+		//nl means assert that array arr.length is less than nl
+		if(arr && n_min && n_max){
 			try{
 				assert.equal(Array.isArray(arr), true)
-				assert.equal(arr.length>ng, true)
-				assert.equal(arr.length<nl, true)
+				assert.equal(arr.length>=n_min, true)
+				assert.equal(arr.length<=n_max, true)
 			}catch(err){
 				return false
 			}
-		}else if(arr && ng){
+		}else if(arr && n_min){
 			try{
 				assert.equal(Array.isArray(arr), true)
-				assert.equal(arr.length>ng, true)
+				assert.equal(arr.length>=n_min, true)
 			}catch(err){
 				return false
 			}
-		}else if(arr && nl){
+		}else if(arr && n_max){
 			try{
 				assert.equal(Array.isArray(arr), true)
-				assert.equal(arr.length<nl, true)
+				assert.equal(arr.length<=n_max, true)
 			}catch(err){
 				return false
 			}
@@ -74,29 +76,61 @@ export class Validation{
 		}else{
 			return false
 		}
-		if(nng && Array.isArray(arr)){
-			for(var i=0; i<arr.length; i++){
-				try{
-					assert.equal(arr[i]>nng, true)
-				}catch(err){
-					return false
-				}
-			}
-		}
-		if(nnl && Array.isArray(arr)){
-			for(var i=0; i<arr.length; i++){
-				try{
-					assert.equal(arr[i]<nnl, true)
-				}catch(err){
-					return false
-				}
-			}
-		}
 		return true
 	}
 
-	isObj(){
+	isStrata(){
+		
+	}
 
+	//w_max = assert that width is less than w_max
+	//w_min = assert that width is greater than w_min
+	//d_max = assert that depth is less than d_max
+	//d_min = assert that depth is greater than d_min
+	//n = current level of depth
+	isObj(obj, w_min, w_max, d_min, d_max, n=0, maxdepth=[0], truth=[true]){
+		if(!truth[0]){return false}
+		if(typeof obj === 'object'){
+			if(w_min||w_max){
+				var i;
+				for(i = 0; i<Object.keys(obj).length; i++){
+					if(d_max){
+						try{
+							assert.equal(n<=d_max, true)
+						}catch{
+							truth[0]=false
+						}
+					}
+					if(n>maxdepth[0]){maxdepth[0]=n}
+					this.isObj(obj[Object.keys(obj)[i]], w_min, w_max, d_min, d_max, n+1, maxdepth, truth)
+				}
+				if(w_min){
+					try{
+						assert.equal(i>=w_min, true)
+					}catch{
+						truth[0]=false
+					}
+				}
+				if(w_max){
+					try{
+						assert.equal(i<=w_max, true)
+					}catch{
+						truth[0]=false
+					}
+				}
+			}
+		}
+
+		if(n==0){
+			if(d_min){
+				try{
+					assert.equal(maxdepth[0]>=d_min)
+				}catch{
+					truth[0]=false
+				}
+			}
+		}
+		return truth[0]
 	}
 
 	isStr(){
