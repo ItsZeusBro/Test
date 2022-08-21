965ff40dfbfdcb60302ffc9de4ba1c2469e3908a
Zeus <zeusofolympus1000@gmail.com>
2022-08-21 14:48:03 -0400

    i think this is statistics

--- Validation/Spirals.js
+++ Validation/Spirals.js
@@ -22,3 +22,11 @@
 //the mean spiral would be the average of the total set of all possible spirals if we were to plot these spirals on a graph
 
 //On a set of examples with mean spirals of their own, one can take the mean spiral of the set of mean spirals
+
+//If there is no pattern in a string of numbers or charachters then it is either removed or aggregated with the other examples
+//If there is no pattern in one example and a pattern in another the one without a pattern can be discarded so long as it is 
+//statistically insignificant
+//if there are no patterns in the entire set of strings, then they can be aggregated and the normal distribution
+//of spirals can be calculated. 
+//There can be other methods for when examples are recurring (like heads or tails) but have no meaning
+//outside of the frequency of their occurence relative to other examples 
