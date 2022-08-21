ba966c759cac6a8ce9a8c7a9b4d2a8db97e716d3
Zeus <zeusofolympus1000@gmail.com>
2022-08-21 13:51:52 -0400

    some definitions for spiral recurrance

---
+++ Validation/Spirals.js
@@ -0,0 +1,12 @@
+//What is a perfect recurrance?
+//Infinite recursion (which is not necessarily a circle but can be averaged as a circle)
+
+//what is a minimal spiral that approaches pure recurrance?
+//A repetition of the original pattern with an off by one in any given direction
+//So aababcabcd... or aazazyazyx...etc 
+//the direction of the off by one value in for a discrete step represents the direction of the spiral
+
+//That is the most extreme theoretical case and the foundation for recurrent pattern analysis
+
+//when fitting a spiral we are trying to fit the largest possible spiral for the data
+//or in the alternative the mean spiral between the largest and the smallest
