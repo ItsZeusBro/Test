6734c667dcfd85a0b7dce8d4803f7505b9e17203
Zeus <zeusofolympus1000@gmail.com>
2022-08-25 23:30:40 -0400

    need randomStrata to use randomObjectArray randomArrayObject

--- Randoms/Randoms.js
+++ Randoms/Randoms.js
@@ -1,5 +1,6 @@
 import { Comparators } from "../Comparators/Comparators.js";
 import {Types} from "../Types/Types.js"
+import * as assert from "node:assert"
 export class Randoms{
 	constructor(){
 		this.comparators = new Comparators()
@@ -18,7 +19,6 @@ export class Randoms{
 
 	randomNull(except){
 		var NullType = this.randomSample([null, undefined, NaN, 0, '0'])
-		
 		if(this.comparators.isEqual(NullType, except)){
 			return this.randomNull(except)
 		}else{
@@ -44,7 +44,7 @@ export class Randoms{
 		var arr=[]; 
 		for(var i=0;i<n;i++){arr.push(this._genString(n))}; 
 		if(this.comparators.isEqual(arr, except)){
-			return this.randomString(n, except)
+			return this.randomStringArray(n, except)
 		}else{
 			return arr
 		}
@@ -96,6 +96,19 @@ export class Randoms{
     // randEnc(n){return "utf8"}
     // randEncArr(n){return ['utf8']}
 
+	randomArrayObject(n, except){
+		var arrObj={}
+		for(var i=0; i<n; i++){
+			arrObj[this.randomString(n)]=this.randomArray(n)
+		}
+
+		if(this.comparators.isEqual(arrObj, except)){
+			return this.randomArrayObject(n, except)
+		}else{
+			return arrObj
+		}
+	}
+
     randomObject(n, except){
 		var obj = this._randomObject(n)
 
@@ -123,7 +136,7 @@ export class Randoms{
 		var obj={}
 			if(n){
 				for(var i=0; i<n; i++){
-					obj[this.randomString(20)]=this._randomObject(n-1)
+					obj[this.randomString(n)]=this._randomObject(n-1)
 				}
 			}
 			return obj
@@ -169,6 +182,7 @@ export class Randoms{
 }
 
 var randoms = new Randoms()
+var comparators = new Comparators()
 var prim;
 for(var i = 0; i<5; i++){
 	prim = randoms.randomPrimitive(5, prim)
@@ -196,14 +210,53 @@ for(var i = 0; i<5; i++){
 
 var integerArray;
 for(var i = 0; i<5; i++){
-	integerArray = randoms.randomIntegerArray(2, integerArray)
+	integerArray = randoms.randomIntegerArray(5, integerArray)
 	console.log(integerArray)
 }
 
-var integerArray;
-for(var i = 0; i<5; i++){
-	integerArray = randoms.randomIntegerArray(2, integerArray)
-	console.log(integerArray)
+var stringArray;
+var prev;
+for(var i = 0; i<10; i++){
+	prev=stringArray
+	stringArray = randoms.randomStringArray(1, stringArray)
+	console.log(prev, stringArray)
+	assert.equal(comparators.isEqual(prev, stringArray), false)
 }
-// console.log(Number.isInteger(0))
 
+var array;
+prev = undefined;
+for(var i = 0; i<10; i++){
+	prev=array
+	array = randoms.randomArray(1, array)
+	console.log(prev, array)
+	assert.equal(comparators.isEqual(prev, array), false)
+}
+
+
+
+var object;
+prev = undefined;
+for(var i = 0; i<100; i++){
+	prev=object
+	object = randoms.randomObject(4, object)
+	console.log(prev, object)
+	assert.equal(comparators.isEqual(prev, object), false)
+}
+
+var objectArray;
+prev = undefined;
+for(var i = 0; i<100; i++){
+	prev=objectArray
+	objectArray = randoms.randomObjectArray(1, objectArray)
+	console.log(prev, objectArray)
+	assert.equal(comparators.isEqual(prev, objectArray), false)
+}
+
+var arrayObject;
+prev = undefined;
+for(var i = 0; i<100; i++){
+	prev=arrayObject
+	arrayObject = randoms.randomArrayObject(3, arrayObject)
+	console.log(prev, arrayObject)
+	assert.equal(comparators.isEqual(prev, arrayObject), false)
+}
